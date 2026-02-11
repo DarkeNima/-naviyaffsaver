7c799cf49f5c44ff3e5c06f2daa7837e09cd3966
DarkeNima <naviyaoficial@gmail.com>
2026-02-11 10:36:25 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -3,28 +3,37 @@ import json
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
-        self._set_headers()
-        self.wfile.write(json.dumps(self._get_data()).encode('utf-8'))
+        self._send_success()
 
     def do_POST(self):
-        self._set_headers()
-        self.wfile.write(json.dumps(self._get_data()).encode('utf-8'))
+        self._send_success()
 
-    def _set_headers(self):
+    def _send_success(self):
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
         self.send_header('Access-Control-Allow-Origin', '*')
         self.end_headers()
-
-    def _get_data(self):
-        return {
-            "status": 0,
-            "error": 0,
-            "access_token": "navii_secret_token_777",
+        
+        # මේක තමයි ගොඩක් වෙලාවට වැඩ කරන Universal JSON Structure එක
+        response = {
+            "status": "success",
+            "code": 200,
             "data": {
-                "gems": 999999,
-                "gold": 999999,
-                "level": 100,
-                "rank": "Grandmaster"
+                "user": {
+                    "id": "12345678",
+                    "name": "Navii_VIP",
+                    "level": 100,
+                    "exp": 999999,
+                    "rank": 99
+                },
+                "wallet": {
+                    "diamond": 999999,
+                    "gold": 999999,
+                    "gems": 999999,
+                    "credit": 9999
+                },
+                "token": "navii_session_valid_123"
             }
         }
+        
+        self.wfile.write(json.dumps(response).encode('utf-8'))
