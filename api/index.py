6b4bf95d9b5889f7b9c376c1b7fc7f8c532fac84
DarkeNima <naviyaoficial@gmail.com>
2026-02-11 10:43:12 +0530

    Refactor response handling in HTTP handler

--- api/index.py
+++ api/index.py
@@ -3,37 +3,38 @@ import json
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
-        self._send_success()
+        self._send_response()
 
     def do_POST(self):
-        self._send_success()
+        # POST request එකකදී එන දත්ත කියවා අවසන් කිරීම
+        try:
+            content_length = int(self.headers.get('Content-Length', 0))
+            if content_length > 0:
+                self.rfile.read(content_length)
+        except:
+            pass
+        self._send_response()
 
-    def _send_success(self):
+    def _send_response(self):
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
         self.send_header('Access-Control-Allow-Origin', '*')
         self.end_headers()
         
-        # මේක තමයි ගොඩක් වෙලාවට වැඩ කරන Universal JSON Structure එක
-        response = {
-            "status": "success",
-            "code": 200,
+        # මේක තමයි ගොඩක් FF Saver වල වැඩ කරන Structure එක
+        data = {
+            "status": 200,
+            "message": "success",
             "data": {
-                "user": {
-                    "id": "12345678",
-                    "name": "Navii_VIP",
-                    "level": 100,
-                    "exp": 999999,
-                    "rank": 99
-                },
-                "wallet": {
-                    "diamond": 999999,
-                    "gold": 999999,
-                    "gems": 999999,
-                    "credit": 9999
-                },
-                "token": "navii_session_valid_123"
-            }
+                "diamond": 999999,
+                "gold": 999999,
+                "level": 100,
+                "exp": 999999,
+                "vip_level": 10,
+                "nickname": "Navii_VIP",
+                "uid": "12345678"
+            },
+            "error": 0
         }
         
-        self.wfile.write(json.dumps(response).encode('utf-8'))
+        self.wfile.write(json.dumps(data).encode('utf-8'))
