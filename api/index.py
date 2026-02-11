9fd66b6003a023104f995bec847cdc5f12c37fa5
DarkeNima <naviyaoficial@gmail.com>
2026-02-11 10:08:07 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -3,13 +3,19 @@ import json
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
+        self._send_response()
+
+    def do_POST(self):
+        self._send_response()
+
+    def _send_response(self):
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
+        # මේක දාන්න ඕනේ Game එකට දත්ත කියවන්න ඉඩ දෙන්න
+        self.send_header('Access-Control-Allow-Origin', '*')
         self.end_headers()
         
-        # ඔයාට අවශ්‍ය Game Data ටික මෙතන තියෙන්නේ
-        # මේ දත්ත වෙනස් කරලා ඔයාට කැමති විදිහට Dashboard එක හදාගන්න පුළුවන්
-        game_data = {
+        data = {
             "status": "success",
             "gems": 999999,
             "gold": 999999,
@@ -19,5 +25,4 @@ class handler(BaseHTTPRequestHandler):
             "message": "Welcome Navii! Your Private Server is Active."
         }
         
-        self.wfile.write(json.dumps(game_data).encode())
-        return
+        self.wfile.write(json.dumps(data).encode())
